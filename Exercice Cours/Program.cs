ab49d6dbdf9c830443755806daac15788ca08611
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-25 09:08:02 +0100

    Exercice Cours

---
+++ Exercice Cours/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Cours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            Console.WriteLine("C# en Lic2 LIAGE");
+            Console.WriteLine("C'est un cours pour tous");
+            Console.WriteLine("Chaque etudiant doit arriver a afficher ce message");
+            Console.ReadLine();
+        }
+    }
+}
