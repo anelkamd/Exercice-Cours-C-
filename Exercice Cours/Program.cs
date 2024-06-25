550cec0801b67afd2d792807915192c9e27b4354
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-25 10:39:43 +0100

    update

--- Exercice Cours/Program.cs
+++ Exercice Cours/Program.cs
@@ -10,10 +10,29 @@ namespace Exercice_Cours
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word");
-            Console.WriteLine("C# en Lic2 LIAGE");
-            Console.WriteLine("C'est un cours pour tous");
-            Console.WriteLine("Chaque etudiant doit arriver a afficher ce message");
+            //String nom = "Anelka";
+            //String prenom = "MD";
+            //int age = 10;
+
+            //Console.WriteLine("Votre Nom : " + nom);
+            //Console.WriteLine("Votre Prenom : " + prenom);
+            //Console.WriteLine("Votre Age : " + age);
+
+            int Nombre1 = 12;
+            int Nombre2 = 22;
+            double div;
+            div = Nombre1 / Nombre2;
+
+            int Somme = Nombre1 + Nombre2;
+            Console.WriteLine("La Somme de " + Nombre1 + " Et "+ Nombre2 + " = "+Somme);
+            Console.WriteLine("=========================");
+            Console.WriteLine("La division de " + Nombre1 + " / " + Nombre2 + " = " + div);
+
+
+
+
+
+
             Console.ReadLine();
             // Coding By Anelka MD
         }
